99717ef4e7a7ede2652222525a7e4e2038f60000
Thales <thalesvon@gmai.com>
2019-05-08 18:15:11 -0300

    Action updated to change both scheduled jobs

--- src/change.py
+++ src/change.py
@@ -16,14 +16,16 @@ def change(event, context):
 
     if data == 'enable':
         cw_events = boto3.client('events',region_name=region)
-        event_rule = cw_events.describe_rule(Name=start_event)
-        if event_rule['State'] == 'ENABLED':
+        start_event_rule = cw_events.describe_rule(Name=start_event)
+        stop_event_rule = cw_events.describe_rule(Name=stop_event)
+        if start_event_rule['State'] == 'ENABLED' or stop_event_rule['State'] == 'ENABLED':
             pass
         else:
             cw_events.enable_rule(Name=start_event)
+            cw_events.enable_rule(Name=stop_event)
 
         body = {
-            "message": "your instances will be started",
+            "message": "Scheduled Jobs have been enabled",
             "input": data
         }
         response = {
@@ -35,14 +37,17 @@ def change(event, context):
     
     elif data == 'disable':
         cw_events = boto3.client('events',region_name=region)
-        event_rule = cw_events.describe_rule(Name=stop_event)
+        start_event_rule = cw_events.describe_rule(Name=start_event)
+        stop_event_rule = cw_events.describe_rule(Name=stop_event)
+        if start_event_rule['State'] == 'DISABLED' or stop_event_rule['State'] == 'DISABLED':
         if event_rule['State'] == 'DISABLED':
             pass
         else:
+            cw_events.disable_rule(Name=start_event)
             cw_events.disable_rule(Name=stop_event)
 
         body = {
-            "message": "your instances will be stopped",
+            "message": "Scheduled Jobs have been disabled",
             "input": data
         }
         response = {
