ed6af7e2019cdd031f040de014ad0689c8e50f34
Thales <thalesvon@gmai.com>
2019-05-13 17:08:36 -0300

    added slack messages

--- src/change.py
+++ src/change.py
@@ -1,14 +1,17 @@
 import json
 import boto3
 import os
+import urllib.request
 
 region = os.environ['region']
 instances = os.environ['instances'].split()
 start_event = os.environ['start_event']
 stop_event = os.environ['stop_event']
+slack_token = os.environ['slack_token']
+channel = os.environ['channel']
 
 def change(event, context):
-    
+        
     try:
         data = event['action']
     except:
@@ -18,14 +21,17 @@ def change(event, context):
         cw_events = boto3.client('events',region_name=region)
         start_event_rule = cw_events.describe_rule(Name=start_event)
         stop_event_rule = cw_events.describe_rule(Name=stop_event)
-        if start_event_rule['State'] == 'ENABLED' or stop_event_rule['State'] == 'ENABLED':
-            pass
+        if start_event_rule['State'] == 'ENABLED' and stop_event_rule['State'] == 'ENABLED':
+            msg = "Scheduled Jobs are already enabled, no changes were made"
         else:
             cw_events.enable_rule(Name=start_event)
             cw_events.enable_rule(Name=stop_event)
+            msg = "Scheduled Jobs have been enabled"
+            req = urllib.request.Request('https://slack.com/api/chat.postMessage?token='+slack_token+'&channel='+channel+'&text=Scheduled%20Jobs%20have%20been%20enabled&as_user=true&pretty=1')
+            resp = urllib.request.urlopen(req)
 
         body = {
-            "message": "Scheduled Jobs have been enabled",
+            "message": msg,
             "input": data
         }
         response = {
@@ -39,15 +45,17 @@ def change(event, context):
         cw_events = boto3.client('events',region_name=region)
         start_event_rule = cw_events.describe_rule(Name=start_event)
         stop_event_rule = cw_events.describe_rule(Name=stop_event)
-        if start_event_rule['State'] == 'DISABLED' or stop_event_rule['State'] == 'DISABLED':
-        if event_rule['State'] == 'DISABLED':
-            pass
+        if start_event_rule['State'] == 'DISABLED' and stop_event_rule['State'] == 'DISABLED':
+            msg = "Scheduled Jobs are already disabled, no changes were made"
         else:
             cw_events.disable_rule(Name=start_event)
             cw_events.disable_rule(Name=stop_event)
+            msg = "Scheduled Jobs have been disabled"
+            req = urllib.request.Request('https://slack.com/api/chat.postMessage?token='+slack_token+'&channel='+channel+'&text=Scheduled%20Jobs%20have%20been%20disabled&as_user=true&pretty=1')
+            resp = urllib.request.urlopen(req)
 
         body = {
-            "message": "Scheduled Jobs have been disabled",
+            "message": msg,
             "input": data
         }
         response = {
