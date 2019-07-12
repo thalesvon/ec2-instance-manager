a00e5fe3aa16a79b44f375569a6b9cdb880b6111
Thales <thalesvon@gmai.com>
2019-07-12 13:02:55 -0300

    adding rds start/stop functionality

--- src/start.py
+++ src/start.py
@@ -4,10 +4,16 @@ import os
 
 region = os.environ['region']
 instances = os.environ['instances'].split()
+rds_clusters = os.environ['rds'].split()
 
 def start(event, context):
     ec2 = boto3.client('ec2', region_name=region)
     ec2.start_instances(InstanceIds=instances)
+
+    rds = boto3.client('rds')
+    for cluster in rds_clusters:
+        rds.start_db_cluster(DBClusterIdentifier=cluster)
+
     body = {
         "message": "Started your instances "+str(instances),
         "input": event
